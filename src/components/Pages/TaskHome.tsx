612ea01ac699c8becf0479c3105dcace5061a322
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2024-11-21 01:37:57 +0100

    fixed some css sizing

--- src/components/Pages/TaskHome.tsx
+++ src/components/Pages/TaskHome.tsx
@@ -1,10 +1,28 @@
 import UserInfo from "../UserInfo/UserInfo";
 import Header from "../Header/Header";
+import { useEffect, useState } from "react";
+import { Api, User } from "../Api.ts";
+import "./TaskHome.scss";
+
 const TaskHome = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const loadUsers = async () => {
+      const users = await Api.getUsers();
+      setUsers(users);
+    };
+    loadUsers();
+  }, []);
+
   return (
     <div>
       <Header />
-      <UserInfo user={{ name: "John Doe", points: 100, id: "userId" }} />
+      <div className={"user-info-container"}>
+        {users.map((user) => (
+          <UserInfo key={user.id} user={user} />
+        ))}
+      </div>
     </div>
   );
 };
