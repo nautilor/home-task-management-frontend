1138e17d7a4a0460cdc6411be7802373b6ff6f1e
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2024-11-22 01:27:18 +0100

    added the possibility to hide the button to add an item on the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,21 +3,29 @@ import { LuPlus } from "react-icons/lu";
 import "./Header.scss";
 
 interface HeaderProps {
-  onClick?: () => void;
+  onAddClick?: () => void;
+  hideAddButton?: boolean;
 }
 
 const Header = (props: HeaderProps) => {
+  const hideAddButton = props.hideAddButton ?? false;
   return (
     <div className={"header-container"}>
       <Text fontSize="2xl" fontWeight={"bold"}>
         Gestione Faccende Domestiche
       </Text>
-      <Button colorPalette={"blue"} variant={"solid"} onClick={props.onClick}>
-        <LuPlus />{" "}
-        <Text fontWeight={"medium"} textStyle={"md"}>
-          Aggiungi
-        </Text>
-      </Button>
+      {!hideAddButton && (
+        <Button
+          colorPalette={"blue"}
+          variant={"solid"}
+          onClick={props.onAddClick}
+        >
+          <LuPlus />{" "}
+          <Text fontWeight={"medium"} textStyle={"md"}>
+            Aggiungi
+          </Text>
+        </Button>
+      )}
     </div>
   );
 };
